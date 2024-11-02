2ba3d48628518e4f4108dcd47ca48023d5e3157c
Mizaan-hub <mizaanafandi@gmail.com>
2024-11-02 18:06:23 +0530

    ppr for startup page started

--- app/(root)/startup/[id]/page.tsx
+++ app/(root)/startup/[id]/page.tsx
@@ -1,3 +1,6 @@
+import { client } from '@/sanity/lib/client'
+import { STARTUP_BY_ID_QUERY } from '@/sanity/lib/queries'
+import { notFound } from 'next/navigation'
 import React from 'react'
 
 export const experimental_ppr = true
@@ -5,8 +8,12 @@ export const experimental_ppr = true
 const page = async({ params } : { params: Promise<{ id: string }>}) => {
 
   const id = (await params).id
+  console.log("Start up id:" ,id)
+  const post = await client.fetch(STARTUP_BY_ID_QUERY, { id })
+  if(!post) return notFound()
+
   return (
-    <>{id}</>
+    <>{post.title}</>
   )
 }
 
