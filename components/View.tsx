379ab4e929df54ec1c6baf2e631465ee1de71a2b
Mizaan-hub <mizaanafandi@gmail.com>
2024-11-09 01:06:37 +0530

    Implemetation of ppr and live views setup complete

--- components/View.tsx
+++ components/View.tsx
@@ -1,30 +1,40 @@
-import { client } from '@/sanity/lib/client';
-import Ping from './Ping';
-import { STARTUP_VIEWS_QUERY } from '@/sanity/lib/queries';
-const View = async ({ id } : { id : string }) => {
-  const { views: totalViews} = await client.withConfig({ useCdn : false }).fetch(STARTUP_VIEWS_QUERY, { id })
+import { client } from "@/sanity/lib/client";
+import Ping from "./Ping";
+import { STARTUP_VIEWS_QUERY } from "@/sanity/lib/queries";
+import { writeClient } from "@/sanity/lib/write-client";
+import { unstable_after as after } from "next/server";
+const View = async ({ id }: { id: string }) => {
+  const { views: totalViews } = await client
+    .withConfig({ useCdn: false })
+    .fetch(STARTUP_VIEWS_QUERY, { id });
 
-  let x = ""
+  let x = "";
 
-  if(totalViews == 1){
-    x = "View"
+  if (totalViews == 1) {
+    x = "View";
   } else {
-    x = "Views"
+    x = "Views";
   }
-  // TODO: Update the number of views
+  after(
+    async () =>
+      await writeClient
+        .patch(id)
+        .set({ views: totalViews + 1 })
+        .commit()
+  );
   return (
     <div className="view-container">
-    <div className="absolute -top-2 -right-2 text-black">
-      <Ping />
+      <div className="absolute -top-2 -right-2 text-black">
+        <Ping />
+      </div>
+
+      <p className="view-text">
+        <span className="font-black text-black">
+          {totalViews} {x}
+        </span>
+      </p>
     </div>
+  );
+};
 
-    <p className="view-text">
-      <span className="font-black text-black">
-        {totalViews} {x}
-      </span>
-    </p>
-  </div>
-  )
-}
-
-export default View
+export default View;
