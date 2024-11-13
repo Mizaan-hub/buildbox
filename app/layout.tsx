e005285bf5cfe1bdd17e6412789a800d472b2ee0
Mizaan-hub <mizaanafandi@gmail.com>
2024-11-14 03:11:25 +0530

    Analytics added

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import localFont from "next/font/local";
 import "./globals.css";
 import 'easymde/dist/easymde.min.css'
 import { Toaster } from "@/components/ui/toaster";
+import { Analytics } from "@vercel/analytics/react"
 
 const workSans = localFont({
   src: [
@@ -74,6 +75,7 @@ export default function RootLayout({
         className={workSans.variable}
       >
         {children}
+        <Analytics />
         <Toaster />
       </body>
     </html>
